340ea27facc8e3b0bcc4fc74617395f40f48dbb2
TurtleTrademark <71402796+TurtleTrademark@users.noreply.github.com>
2021-04-27 11:38:10 -0400

    Add files via upload

---
+++ DemoPatcher.bat
@@ -0,0 +1,44 @@
+@ECHO OFF
+
+IF EXIST "root" (
+ECHO Please remove "root" folder.
+PAUSE
+EXIT
+)
+
+IF EXIST "%1" ( GOTO :HASFILE ) ELSE ( GOTO :NOFILE )
+
+:HASFILE
+GOTO :MAIN
+
+:NOFILE
+ECHO Please drag a Pikmin 2 Disk Image (*.iso or *.gcm) onto the .bat file.
+PAUSE
+EXIT
+
+
+:MAIN
+ECHO Welcome to the Pikmin 2 Demo Patcher!
+ECHO Extracting Pikmin 2 Disk Image... (this may take a while)
+gcr.exe %1 "root" e "."
+ECHO Disk extracted...
+
+CHOICE /C "UPJ" /M "What Demo Disk Version would you like? (U = USA Demo 1 | P = PAL Demo | J = JPN Demo)"
+
+If %errorlevel% == 1 (XCOPY /E/I/Y "dp\&&systemdataUS"       "root\&&systemdata"
+XCOPY /E/I/Y "dp\openingUS.bnr" 		"root\opening.bnr" 
+XCOPY /E/I/Y "dp\pikmin2UP.map" 		"root\pikmin2UP.map")
+
+If %errorlevel% == 2 (XCOPY /E/I/Y "dp\&&systemdataPAL"       "root\&&systemdata"
+XCOPY /E/I/Y "dp\openingPAL.bnr" 		"root\opening.bnr"
+XCOPY /E/I/Y "dp\boot_pal.szs" "root\user\Yamashita\arc\boot_pal.szs")
+
+If %errorlevel% == 3 (XCOPY /E/I/Y "dp\&&systemdataJPN"       "root\&&systemdata"
+XCOPY /E/I/Y "dp\openingJPN.bnr" 		"root\opening.bnr"
+XCOPY /E/I/Y "dp\boot_jp.szs" "root\user\Yamashita\arc\boot_jp.szs")
+
+ECHO Now building new disk image file...
+gcr.exe "root" "Pikmin 2 [Patched]%~x1"
+rd /s /q "root"
+ECHO Done! Enjoy! :3
+pause
---
+++ ReadMe.md
@@ -0,0 +1,26 @@
+
+______ _ _              _         _____  ______                      ______     _       _               
+| ___ (_) |            (_)       / __  \ |  _  \                     | ___ \   | |     | |              
+| |_/ /_| | ___ __ ___  _ _ __   `' / /' | | | |___ _ __ ___   ___   | |_/ /_ _| |_ ___| |__   ___ _ __ 
+|  __/| | |/ / '_ ` _ \| | '_ \    / /   | | | / _ \ '_ ` _ \ / _ \  |  __/ _` | __/ __| '_ \ / _ \ '__|
+| |   | |   <| | | | | | | | | | ./ /___ | |/ /  __/ | | | | | (_) | | | | (_| | || (__| | | |  __/ |   
+\_|   |_|_|\_\_| |_| |_|_|_| |_| \_____/ |___/ \___|_| |_| |_|\___/  \_|  \__,_|\__\___|_| |_|\___|_|   
+
+ 
+Welcome to the Pikmin 2 USA Demo 1 Patcher by TurtleTrademark. 
+This patcher will help you patch any Final Pikmin 2 Disk Image (USA, PAL, JP) into USA Demo 1.
+For ease of access I have also included the symbol map for your convenience. 
+
+Download Link: https://sites.google.com/view/turtleshacks/hacking-tools
+
+Instructions:
+1. Drag and drop your Pikmin 2 ISO into the same directory/folder as the "DemoPatcher.bat" file. 
+2. Drag and drop your Pikmin 2 ISO onto the "DemoPatcher.bat" file.
+3. Wait for the installer to finish.
+4. Enjoy!
+
+Changelog: 
+2.0
+- Add support for JPN and PAL Demo
+- Code cleanup
+
